36f3872fb87962649bf12407afb6a1dcc915b8b3
Dima <81911982+dimaTidev@users.noreply.github.com>
2025-04-04 14:44:02 -0400

    Changed Header and Footer to Server components

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -1,31 +1,12 @@
-'use client';
-
 import Styles from "./footer.module.css";
 import { Suspense } from "react";
 import Socials from "@/lib/socials/socials";
-import ActionButton from "@/lib/UIComponents/ActionButton";
-import { Size, Variant } from "@/lib/UIComponents/uiCommon";
-import Link from "next/link";
-import { Modal, ModalContent } from "@/lib/floatingUI/modal";
-import { DialogDescriptionArea, DialogHeading } from "@/lib/floatingUI/dialog";
-import { gql, useSuspenseQuery } from "@apollo/client";
+import { gql } from "@apollo/client";
 import Fade from "@/lib/UIComponents/fadeIn";
 import Skeleton from "react-loading-skeleton";
+import apolloServerClient from "@/lib/apollo/apolloServerClient";
 
-const GET_PERSON = gql`
-  query GetPerson {
-    AboutMe(id: "aboutMe") {
-      person {
-        email
-        socialLinks
-      }
-      resumeUrl
-    }
-  }
-`;
-
-export default function Footer({className}) {
-
+export default async function Footer({className}) {
   return (
     <div className={`${Styles.base} ${className}`}>
         <div className={Styles.container}>
@@ -55,46 +36,45 @@ function FooterLoading(){
     )
 }
 
-function FooterContent(){
-  console.log("Render footer");
-  
-  // return;
-    const { data: queryData } = useSuspenseQuery(GET_PERSON);
-    // const [isContactFormOpen, setContactFormOpen] = useState(false);
+async function FooterContent(){
+    const dataRes = await apolloServerClient.query({
+      query: GET_PERSON,
+    });
 
-    const data = queryData.AboutMe;
+    const data = dataRes?.data?.AboutMe;
 
     const socialLinks = [];
-    if(data.person.email){
+    if(data?.person?.email){
       socialLinks.push(data.person.email);
     }
-    socialLinks.push(...data.person.socialLinks);
+    if(data?.person?.socialLinks){
+      socialLinks.push(...data.person.socialLinks);
+    }
 
-  return (
-    <>
-        <div className="u-layout_flex-column gap-xxl">
-            <div className="u-layout_flex-row gap-l">
-                {/* {data.resumeUrl && (
-                    <Link href={data.resumeUrl}>
-                        <ActionButton src="icons/download.svg" size={Size.S} variant={Variant.SECONDARY}>Resume</ActionButton>
-                    </Link>
-                )} */}
-                <Socials linksArray={socialLinks}/>
-            </div>
-            
-            {/* <hr/>
-
-            <ActionButton size={Size.S} variant={Variant.PRIMARY} onClick={() => setContactFormOpen(true)}>Contact me</ActionButton>
-    
-            <Modal open={isContactFormOpen} onOpenChange={setContactFormOpen}>
-                <ModalContent>
-                <DialogHeading>Contact form</DialogHeading>
-                <DialogDescriptionArea>
-                    <ContactForm/>
-                </DialogDescriptionArea>
-                </ModalContent>
-            </Modal> */}
-        </div>
-    </>
-  )
+    return (
+      <>
+          <div className="u-layout_flex-column gap-xxl">
+              <div className="u-layout_flex-row gap-l">
+                  {/* {data.resumeUrl && (
+                      <Link href={data.resumeUrl}>
+                          <ActionButton src="icons/download.svg" size={Size.S} variant={Variant.SECONDARY}>Resume</ActionButton>
+                      </Link>
+                  )} */}
+                  <Socials linksArray={socialLinks}/>
+              </div>
+          </div>
+      </>
+    )
 }
+
+const GET_PERSON = gql`
+  query GetPerson {
+    AboutMe(id: "aboutMe") {
+      person {
+        email
+        socialLinks
+      }
+      resumeUrl
+    }
+  }
+`;
--- src/lib/socials/socials.jsx
+++ src/lib/socials/socials.jsx
@@ -1,5 +1,3 @@
-'use client'
-
 import Styles from './social.module.css'
 
 /**
