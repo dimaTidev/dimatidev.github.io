e36ed184c2f22217550d63a17d2fbf8d60528216
Dima <81911982+dimaTidev@users.noreply.github.com>
2025-04-08 13:35:22 -0400

    Fixed the showMore component

--- src/Components/showMoreSection.jsx
+++ src/Components/showMoreSection.jsx
@@ -9,7 +9,9 @@ export default function ShowMoreSection({children, ...params}) {
     const [isExpanded, setExpanded] = useState(false)
   return (
     <div className={`${Styles.notExpanded} ${isExpanded && Styles.expanded}`} {...params}>
-        <div className={`${!isExpanded && Styles.gradientTransparemt} ${Styles.contentBlock}`}>
+        <div 
+          className={`${!isExpanded && Styles.gradientTransparemt} ${!isExpanded && Styles.notExpandedHeight} ${Styles.contentBlock}`} 
+        >
             {children}
         </div>
 
