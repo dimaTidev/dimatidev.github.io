61fd6b05467bc37f6335f0b04b39c460b6b26d1d
Dima <81911982+dimaTidev@users.noreply.github.com>
2025-04-08 13:53:00 -0400

    CleanUp

--- src/app/projectDetails/swiperGallery.jsx
+++ src/app/projectDetails/swiperGallery.jsx
@@ -18,7 +18,6 @@ import Image from 'next/image';
 import Button from '@/lib/UIComponents/Button';
 import {IconArrowLeft, IconArrowRight } from '@/lib/icons/icons';
 import { Variant } from '@/lib/UIComponents/uiCommon';
-import Fade from '@/lib/UIComponents/fadeIn';
 
 export default function SwiperGallery({items}) {
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
